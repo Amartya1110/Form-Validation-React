656a5e50e2284a3350ee98bb80c2728faee1c7d5
Amartya1110 <amartyakhan999@gmail.com>
2024-04-17 16:28:03 +0530

    Initial set-up of our form-validation app

---
+++ index.js
@@ -0,0 +1,10 @@
+import ReactDOM from  "react-dom/client"
+
+// Import CSS
+import "./index.css"
+
+// Importing components
+import App from "./src/App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Import CSS
+import "./App.css"
+
+const App = () => {
+
+    return (
+        <div className="app-layout">
+            App
+        </div>
+    )
+}
+
+export default App
